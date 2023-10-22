74198d32359cce5cc292d84a4072397e1bfcd47e
Michael Williams de la Bastida <m.i.w@outlook.com>
2023-10-22 15:24:27 +0100

    First Commit
    Created dog.py, created first_file with instructions, created ignore file

---
+++ first_file.py
@@ -0,0 +1,13 @@
+# Test working with GitHub Desktop
+
+# Create a virtual environment in your repostory folder
+# my_test_repo/venv/
+# yours can be called anything you like.
+
+# Create a file called ".gitignore"
+# add the line below to .gitignore
+venv/ #<name of your virtual environment folder>
+
+# Create a python file and save this into the repo location
+# mine is called "my_test_repo"
+# this is mine, it's called "first_file.py"
