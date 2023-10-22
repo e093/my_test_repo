200ac907686c9a8c7aaaa267d1a30843a7e7ef82
Michael Williams de la Bastida <m.i.w@outlook.com>
2023-10-22 15:33:03 +0100

    add sit method to dog
    Added is_sitting attribute
    Added sit method

--- dog.py
+++ dog.py
@@ -3,6 +3,14 @@ class Dog:
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
+        self.is_sitting = False
         
     def bark(self):
         print("Woof!")
+        
+    def sit(self):
+        if self.is_sitting:
+            print(f"{self.name} is already sitting!")
+        else:
+            self.is_sitting = True
+            print(f"{self.name} sits down.")
